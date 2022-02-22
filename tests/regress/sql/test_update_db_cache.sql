a1ec2f343a9f8fdfaa9da9464652f3ffb5238dc6
Xuebin Su <sxuebin@vmware.com>
2022-02-22 08:30:03 +0800

    Fix flaky case

--- tests/regress/sql/test_update_db_cache.sql
+++ tests/regress/sql/test_update_db_cache.sql
@@ -23,6 +23,13 @@ DISTRIBUTED BY (i);
 
 CREATE EXTENSION diskquota;
 
+-- Sleep until the worker adds the current db to cache so that it can be found 
+-- when DROP EXTENSION.
+-- FIXME: We cannot use wait_for_worker_new_epoch() here because 
+-- diskquota.state is not clean. Change sleep() to wait() after removing
+-- diskquota.state
+SELECT pg_sleep(1);
+
 -- Should find nothing since t_no_extension is not recorded.
 SELECT diskquota.diskquota_fetch_table_stat(0, ARRAY[]::oid[])
 FROM gp_dist_random('gp_id');
